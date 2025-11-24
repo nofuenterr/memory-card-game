f0561d682f81706533a6ee3a6ff1631445ac5070
RR <rrn091301@gmail.com>
2025-11-24 19:26:52 +0800

    feat: replace placeholder utility menu items with new ones with icons and styles and temporarily remove leaderboard item

--- src/components/UtilityMenu.jsx
+++ src/components/UtilityMenu.jsx
@@ -1,20 +1,22 @@
+import GithubIcon from '../assets/icons/GithubIcon'
+import SettingsIcon from '../assets/icons/SettingsIcon'
 import '../styles/UtilityMenu.css'
 
 function UtilityMenu({ handleSettingsModal }) {
 
   return (
     <div className="utility-menu">
-      <a className="github-link" href="https://github.com/nofuenterr">
-        Github
-      </a>
-      <button className="leaderboard">
-        Leaderboards
-      </button>
-      <button 
-        onClick={() => handleSettingsModal(true)} 
-        className="settings"
+      <a 
+        className="github-link utility-menu__item" 
+        href="https://github.com/nofuenterr/memory-card-game"
       >
-        Settings
+        <GithubIcon />
+      </a>
+      <button
+        onClick={() => handleSettingsModal(true)}
+        className="settings utility-menu__item"
+      >
+        <SettingsIcon />
       </button>
     </div>
   )
