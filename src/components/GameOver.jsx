5151203c6a5dc4f85c84badcc5852dffceac834a
RR <rrn091301@gmail.com>
2025-11-25 01:57:58 +0800

    feat: add gameover modal styles

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,14 +1,20 @@
+import '../styles/GameOver.css'
+
 function GameOver({ ref, best, score, result, setGameState, handleModal }) {
 
   return (
-    <dialog ref={ref} className="modal">
-      <p>{result ? 'You win!' : 'You lose'}</p>
-      <p>Best: {best}</p>
-      <p>Score: {score}</p>
-      <button onClick={() => setGameState('menu')} className="back-to-menu">Menu</button>
-      <button onClick={() => {
-        handleModal(false)
-      }} className="retry">Retry</button>
+    <dialog ref={ref} className="gameover-modal">
+      <div className="gameover-wrapper">
+        <p className='gameover-title'>{result ? 'You win!' : 'You lose'}</p>
+        <p className='gameover-item-wrapper'><span>Best:</span><span>{best}</span></p>
+        <p className='gameover-item-wrapper'><span>Score:</span><span>{score}</span></p>
+        <button onClick={() => 
+          setGameState('menu')
+        } className="back-to-menu gameover-item-button">Menu</button>
+        <button onClick={() => {
+          handleModal(false)
+        }} className="retry gameover-item-button">Retry</button>
+      </div>
     </dialog>
   )
 }
