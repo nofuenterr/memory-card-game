b251fe4ee6c52d0b6d08854ec3dd0eaf09b1980c
RR <rrn091301@gmail.com>
2025-11-24 21:01:10 +0800

    feat: add styles and closedby prop with value of any to settings modal

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,17 +1,30 @@
+import '../styles/Settings.css'
+
 function Settings({ ref, setGameState, handleSettingsModal }) {
   return (
-    <dialog ref={ref} className="settings-modal">
-      <button>Music</button>
-      <button>SFX</button>
-      <button>Help</button>
-      <button onClick={() => {
-        handleSettingsModal(false)
-        setGameState('menu')
-      }} className="back-to-menu">Menu</button>
-      <button onClick={() => 
-        handleSettingsModal(false)} 
-        className="close-settings"
-      >Close</button>
+    <dialog ref={ref} className="settings-modal" closedby="any">
+      <div className="settings-wrapper">
+        <p className='settings-title'>Settings</p>
+        <div className='settings-item-wrapper'>
+          <label htmlFor="music">Music</label>
+          <input id='music' className='music' name='music' type="checkbox" />
+        </div>
+        <div className='settings-item-wrapper'>
+          <label htmlFor="sound">Sound</label>
+          <input id='sound' className='sound' name='sound' type="checkbox" />
+        </div>
+        <button className='help settings-item-button'>
+          Help
+        </button>
+        <button onClick={() => {
+          handleSettingsModal(false)
+          setGameState('menu')
+        }} className="back-to-menu settings-item-button">Menu</button>
+        <button onClick={() =>
+          handleSettingsModal(false)}
+          className="close settings-item-button"
+        >Close</button>
+      </div>
     </dialog>
   )
 }
